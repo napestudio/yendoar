ed40e6968f8f5ae8d4e2e32ca950754d6d804b38
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-09-10 19:14:18 -0300

    codigo seller

--- app/dashboard/nuevo-codigo/page.tsx
+++ app/dashboard/nuevo-codigo/page.tsx
@@ -5,13 +5,16 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import { ChevronLeft } from "lucide-react";
 import CreateCodeForm from "../components/create-discount-code/create-discount-code";
-import { getEventsByUserId } from "@/lib/api/eventos";
+import { getAllEvents, getEventsByUserId } from "@/lib/api/eventos";
 import { authOptions } from "@/app/api/auth/[...nextauth]/options";
 
 export default async function NewCode() {
   const session = await getServerSession(authOptions);
   const { id } = await getUserByEmail(session?.user?.email as string);
-  const events = await getEventsByUserId(id);
+  const events =
+    session?.user.type === "SELLER"
+      ? await getEventsByUserId(id)
+      : await getAllEvents();
   return (
     <>
       <h1 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
