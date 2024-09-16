7a9ecdffa27d0529c67e9d20814a5c1f647bd1b6
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-09-16 00:23:37 -0300

    push logs

--- lib/actions.ts
+++ lib/actions.ts
@@ -259,7 +259,11 @@ export async function getMercadPagoUrl(
     throw new Error("Error MercadoPago");
   }
   if (preference) {
-    redirect(preference.init_point!);
+    if (process.env.NODE_ENV === "production") {
+      redirect(preference.init_point!);
+    } else {
+      redirect(preference.sandbox_init_point!);
+    }
   }
 }
 
