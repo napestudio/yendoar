e50c2302526b7f4ad3dfd4e6ce31b5c28e4d4537
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-10-07 19:40:18 -0300

    prisma fix

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -6,14 +6,28 @@ declare global {
   var cachedPrisma: PrismaClient;
 }
 
-let prisma: PrismaClient;
-if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
-} else {
-  if (!global.cachedPrisma) {
-    global.cachedPrisma = new PrismaClient();
-  }
-  prisma = global.cachedPrisma;
-}
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
+declare const globalThis: {
+  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+} & typeof global;
+
+const prisma = globalThis.prismaGlobal ?? prismaClientSingleton();
 
 export const db = prisma;
+
+if (process.env.NODE_ENV !== "production") globalThis.prismaGlobal = prisma;
+
+// let prisma: PrismaClient;
+// if (process.env.NODE_ENV === "production") {
+//   prisma = new PrismaClient();
+// } else {
+//   if (!global.cachedPrisma) {
+//     global.cachedPrisma = new PrismaClient();
+//   }
+//   prisma = global.cachedPrisma;
+// }
+
+// export const db = prisma;
