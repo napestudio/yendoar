86d9e83d297000f655b3f28945a8690df86ce01e
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-10-15 18:13:30 -0300

    Mueve consulta dentro del condicional

--- app/api/mp-notify/route.tsx
+++ app/api/mp-notify/route.tsx
@@ -2,16 +2,6 @@ import { NextRequest, NextResponse } from "next/server";
 import { MercadoPagoConfig, Payment } from "mercadopago";
 import { getMercadoPagoTokenByUser, payOrderHandler } from "@/lib/actions";
 
-// ESTE TOKEN PUEDE SER EL GLOBAL DE LA MARCA,
-// NO NECESITA SER EL DEL USUARIO Y BASICAMENTE
-// ACA NO PODEMOS CONSEGUIR EL DEL USUARIO,
-// LO USAMOS SOLO PARA VER EL ESTADO DEL PAGO
-// const client = new MercadoPagoConfig({
-//   accessToken:
-//     "APP_USR-7783662794333159-091016-072a36241a8186630dc77a216b492fa5-265508758",
-// });
-// PUSE EL TOKEN ACA PORQUE DESDE EL ENV LO ESTABA TOMANDO MAL Y ESO PUEDE SER EL PROBLEMA DEL FALLO
-
 export async function POST(req: NextRequest, res: NextResponse) {
   const r = await req.json();
 
@@ -19,16 +9,16 @@ export async function POST(req: NextRequest, res: NextResponse) {
   const userId = searchParams.get("user_id");
   const topic = r.topic || r.type;
 
-  const MP_ACCESS_TOKEN = await getMercadoPagoTokenByUser(userId!);
-  const client = new MercadoPagoConfig({
-    accessToken: MP_ACCESS_TOKEN!,
-  });
-
   try {
     if (topic === "payment") {
       if (!userId) {
         return NextResponse.json({ msg: "No user id" });
       }
+
+      const MP_ACCESS_TOKEN = await getMercadoPagoTokenByUser(userId!);
+      const client = new MercadoPagoConfig({
+        accessToken: MP_ACCESS_TOKEN!,
+      });
 
       const paymentId = r.data.id;
 
