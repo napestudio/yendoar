796495abb77fe10f31fc49a0aa6b424ec243cd70
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-10-19 20:25:27 -0300

    WIP

--- app/api/mercadopago/pagos/route.ts
+++ app/api/mercadopago/pagos/route.ts
@@ -1,4 +1,18 @@
+import mpApi, { mercadopago } from "@/lib/api/mercadopago";
+import { Payment } from "mercadopago";
+
 export async function POST(req: Request) {
-  console.log(req);
+  // Obtenemos el cuerpo de la petición que incluye información sobre la notificación
+  const body: { data: { id: string } } = await req.json();
+
+  // Obtenemos el pago
+  const payment = await new Payment(mercadopago).get({ id: body.data.id });
+
+  console.log(payment);
+
+  if (payment.status === "approved") {
+    // ACTUALIZA LA ORDEN
+    // LOS DATOS DE LA ORDEN VIENEN EN metadata.order_id
+  }
   return new Response(null, { status: 200 });
 }
