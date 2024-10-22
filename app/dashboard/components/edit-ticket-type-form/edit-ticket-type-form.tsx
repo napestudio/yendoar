fba13b0b0f976041d4932832f1f7c28cb9c0df31
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-10-21 21:28:09 -0300

    Elimina console log, corrige error estetico en form de tickets

--- app/dashboard/components/edit-ticket-type-form/edit-ticket-type-form.tsx
+++ app/dashboard/components/edit-ticket-type-form/edit-ticket-type-form.tsx
@@ -290,7 +290,7 @@ export default function EditTycketTypeForm({
           )}
         />
         <div className="flex flex-col items-center justify-between rounded-lg border p-4">
-          <div className="flex flex-row items-center justify-between">
+          <div className="flex flex-row items-center justify-between w-full flex-1">
             <div className="space-y-0.5">
               <Label>Descuento</Label>
               <FormDescription>
