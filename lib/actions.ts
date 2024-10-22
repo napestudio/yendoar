81f47a7b71e207e27ecd346f253bb2fdc12d75aa
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-10-21 21:07:06 -0300

    Elimina comentario email

--- lib/actions.ts
+++ lib/actions.ts
@@ -322,8 +322,7 @@ export async function createTicketOrder(tickets: TicketOrderType[]) {
   try {
     const result = await TicketOrders.createTicketOrder(tickets);
     if (result.length > 0) {
-      console.log("TICKETS CREADOS");
-      // return await sendTicketMail(result as TicketOrderType[]);
+      return await sendTicketMail(result as TicketOrderType[]);
     }
   } catch (error) {
     throw new Error("Error tickets");
