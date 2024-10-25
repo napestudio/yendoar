8e12b3f90ac8fb1ab3fbb7e16a09700d11bc73e7
Renzo Costarelli <renzo.costarelli@gmail.com>
2024-10-24 22:33:05 -0300

    merge rebase conflicts

--- components/mp-button/mp-button-server.tsx
+++
@@ -1,7 +0,0 @@
-import { Product } from "@/types/product";
-import MpButton from "./mp-button";
-import { getMercadPagoUrl } from "@/lib/actions";
-
-export default async function MpButtonServer() {
-  return <MpButton />;
-}
