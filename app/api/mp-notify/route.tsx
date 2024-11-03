2c9b8a56f194e386510d1b0f35e8cde04b9276c2
cuerojr <cannibal5033@gmail.com>
2024-11-03 20:15:28 -0300

    fix: added await to handler

--- app/api/mp-notify/route.tsx
+++ app/api/mp-notify/route.tsx
@@ -26,7 +26,7 @@ export async function POST(req: Request) {
       const payment = await new Payment(mp).get({ id: body.data.id });
 
       if (payment.status === "approved") {
-        payOrderHandler(payment.metadata.order_id);
+        await payOrderHandler(payment.metadata.order_id);
       }
     }
   }
