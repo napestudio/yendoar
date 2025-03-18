e46c64269239895cd7d6e487428a57043c4ace71
Renzo Costarelli <renzo.costarelli@gmail.com>
2025-03-18 14:10:32 -0300

    Modifica type user invitations

--- types/user-invitations.ts
+++ types/user-invitations.ts
@@ -1,9 +1,12 @@
+import { UserType } from "./user";
+
 export interface UserInvitation {
   id?: string;
   email: string;
   inviterId: string;
   token: string;
   createdAt?: string | Date | undefined;
+  userType?: UserType | null;
   expiresAt: string | Date;
   updatedAt?: string | Date;
   accepted?: boolean;
