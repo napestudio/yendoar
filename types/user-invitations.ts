dafcff02c5b34e2ab610f3dadf1c15f7cb0eafdb
Renzo Costarelli <renzo.costarelli@gmail.com>
2025-03-20 16:52:00 -0300

    agrega tipo de usuario y modifica formulario invitacion

--- types/user-invitations.ts
+++ types/user-invitations.ts
@@ -6,6 +6,7 @@ export interface UserInvitation {
   inviterId: string;
   token: string;
   createdAt?: string | Date | undefined;
+  role?: UserType | null;
   userType?: UserType | null;
   expiresAt: string | Date;
   updatedAt?: string | Date;
