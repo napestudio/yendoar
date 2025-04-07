df9eec2bd80271e46d01b391bb166caf21b3f043
cuerojr <cannibal5033@gmail.com>
2025-04-07 19:01:21 -0300

    wip: remueve hooks de uploadthing

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,10 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-import { generateReactHelpers } from "@uploadthing/react/hooks";
-import type { OurFileRouter } from "@/app/api/uploadthing/core";
-export const { useUploadThing, uploadFiles } =
-  generateReactHelpers<OurFileRouter>();
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
